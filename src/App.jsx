0084291f90526ab5a7a58f6cc717e3d05b426472
Mahidur Rahman <mahidur952697@gmail.com>
2025-09-30 17:58:48 +0600

    body done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Body from './Components/Body'
 
 function App() {
 
 
   return (
     <>
+    <section>
       <Navbar>  </Navbar>
+    </section>
+    <section>
+      <Body> 
+        Kitchen Room
+      </Body>
+    </section>
+      
     </>
   )
 }
